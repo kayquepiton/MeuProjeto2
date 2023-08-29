3a594bc0908f603c948c604bd7a6fa2154a3f37d
kayquepiton <kayquepiton@gmail.com>
2023-08-29 12:31:00 -0300

    MeuProjeto

--- codAula6.cpp
+++ codAula6.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    for (int n = 48; n <= 57; n++) {
+    for (int n = 0; n <= 127; n++) {
         char asciiChar = static_cast<char>(n + '0'); // Convierte el número a su valor ASCII
         cout << "Decimal: " << n << ", ASCII: " << asciiChar <<  ", Hexadecimal: " << std::hex << n  << ", Octagonal: " << std::oct << n << endl;
     }
