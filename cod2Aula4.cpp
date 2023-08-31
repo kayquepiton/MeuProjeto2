6e5148848c6cd8ffab819733e4929e1db66b5040
kayquepiton <kayquepiton@gmail.com>
2023-08-31 09:01:46 -0300

    MeuProjeto

--- cod2Aula4.cpp
+++ cod2Aula4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <limits>
+#include <iomanip>
 
 using namespace std;
 
@@ -8,10 +9,12 @@ int main(void){
     float valorMaximo = numeric_limits<float>::max();
 
     float pif = 3.141592653589793238462643383279502884197169;
+    double pid = 3.141592653589793238462643383279502884197169;
 
     cout << "Valor máximo de casas decimais de float: " << valorMaximo << endl;
 
-    cout << "Casas pi: " << pif <<endl;
+    cout << "Casas pif: " << fixed << setprecision(numeric_limits<float>::digits10 + 1) << pif << endl;
+    cout << "Casas pid: " << fixed << setprecision(numeric_limits<double>::digits10 + 1) << pid << endl;
 
     return 0;
 }
