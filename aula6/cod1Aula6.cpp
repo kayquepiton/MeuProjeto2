9254cc6245001c57429088d4af3e7cc4b9ce92df
kayquepiton <kayquepiton@gmail.com>
2023-09-01 16:10:22 -0300

    MeuProjeto

---
+++ aula6/cod1Aula6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /*for (int n = 0; n <= 127; n++) {
+        char asciiChar = n; // Converte o número em seu valor na tabela ASCII
+        cout << "ASCII: " << asciiChar << endl;
+    }*/
+
+    return 0;
+}
