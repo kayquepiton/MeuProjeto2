da14429a16dd22dafeb46ec0f5a08f91e55d15af
kayquepiton <kayquepiton@gmail.com>
2023-09-02 21:18:06 -0300

    MeuProjeto

--- aula6/cod11Aula6.cpp
+++ aula6/cod11Aula6.cpp
@@ -3,40 +3,40 @@
 using namespace std;
 
 int main() {
-    // Leia a informação genética de uma planta na forma de um unsigned char
-    unsigned char infoGenetica;
-    cout << "Digite a informação genética da planta (um número de 0 a 255): ";
-    cin >> infoGenetica;
-
-    // Utilize operadores bit a bit para identificar quantos genes estão presentes
+    int informacaoGenetica;
+    
+    // Leitura da informação genética da planta como um caractere sem sinal
+    cout << "Digite a informação genética da planta (um valor entre 0 e 255): ";
+    cin >> informacaoGenetica;
+    
+    // Inicialização de uma variável para contar os genes presentes
     int genesPresentes = 0;
+    
+    // Contagem dos genes presentes usando operadores bit a bit
     for (int i = 0; i < 8; i++) {
-        // Verifica cada bit individualmente
-        if ((infoGenetica & (1 << i)) != 0) {
+        if (informacaoGenetica & (1 << i)) {
             genesPresentes++;
         }
     }
-
-    // Imprima a quantidade de genes presentes
-    cout << "Número de genes presentes nesta planta: " << genesPresentes << endl;
-
-    /*// Solicite do usuário qual gene específico ele quer informações
+    
+    // Exibir o número de genes presentes
+    cout << "Número de genes presentes na planta: " << genesPresentes << endl;
+    
+    // Solicitação da informação sobre um gene específico
     int geneDesejado;
     cout << "Digite o número do gene que você deseja verificar (1 a 8): ";
     cin >> geneDesejado;
-
-    // Verifique se o gene específico está presente
+    
+    // Verifica se o gene desejado está presente na planta
     if (geneDesejado >= 1 && geneDesejado <= 8) {
-        // Desloca o bit apropriado para verificar a presença do gene
-        bool presente = ((infoGenetica & (1 << (geneDesejado - 1))) != 0);
-        if (presente) {
-            cout << "O gene " << geneDesejado << " está presente nesta planta." << endl;
+        if (informacaoGenetica & (1 << (geneDesejado - 1))) {
+            cout << "O gene " << geneDesejado << " está presente na planta." << endl;
         } else {
-            cout << "O gene " << geneDesejado << " não está presente nesta planta." << endl;
+            cout << "O gene " << geneDesejado << " não está presente na planta." << endl;
         }
     } else {
-        cout << "Número de gene inválido. Deve ser entre 1 e 8." << endl;
-    }*/
-
+        cout << "Número de gene inválido. Deve ser um valor entre 1 e 8." << endl;
+    }
+    
     return 0;
 }
