93ab908ba95e21a78b9dc63b638c535f813a4842
kayquepiton <kayquepiton@gmail.com>
2023-09-05 10:20:25 -0300

    MeuProjeto

--- aula9/cod1Aula9.cpp
+++ aula9/cod1Aula9.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 int main() {
-     
-    for(int i = 1; i <= 100; i++){
-        cout << "i = " << i << endl;
+    
+    for(int i = 1; i <= 100; i++){    
+        cout << "\n" << i;
 
-        if (i % 3 == 0){
-            cout << "Fizz" << endl;
-        }
-
-        if (i % 5 == 0){
-            cout << "Buzz" << endl;
+        // Se multiplo de 3 e de 5
+        if (i % 3 == 0 && i % 5 == 0){
+            cout << " - FizzBuzz";
+        }else if (i % 3 == 0){ // Se multiplo de 3
+            cout << " - Fizz";
+        }else if (i % 5 == 0){ // Se multiplo de 5
+            cout << " - Buzz";
         }
     }
     
