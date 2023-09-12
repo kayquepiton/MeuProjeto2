892695d9c34f34fc685efcab72a7a24ef659c252
kayquepiton <kayquepiton@gmail.com>
2023-09-12 12:39:35 -0300

    MeuProjeto

--- aula12/cod5Aula12.cpp
+++ aula12/cod5Aula12.cpp
@@ -23,12 +23,21 @@ int main(){
 void intercala_vetor(int vetor1[], int vetor2[], int n){
 
     int n_total = n * 2;
-    int vetor_intercalado;
+    int vetor_intercalado[n_total];
 
     for (int i = 0; i < n_total; i++){
         if(i < n){
             vetor_intercalado[i] = vetor1[i];
         }else{
+
+            int meio = 0;
+
+            if( n % 2 == 0){
+                meio = (n/2) + 1;
+            }else{
+                meio = n/2 + 1;
+            }
+
             vetor_intercalado[i] = vetor2[i/2];
         }
         
